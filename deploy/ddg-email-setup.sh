60587da1046e52c6eb8c63a4ac52371c48819a68
Carlos Sola-Llonch <csolallo@gmail.com>
2026-04-07 14:08:05 -0400

    initial commit

---
+++ deploy/ddg-email-setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# $1 : verbose flag
+function prepare_app_build() {
+    local verbose="$1"
+
+    create_driver_script "$verbose"
+}
+
+# $1 : verbose flag
+function create_driver_script() {
+    local verbose="$1"
+
+    sc=$(cat <<EOF
+#!/data/data/com.termux/files/usr/bin/bash
+
+token=$(cat ./duckduckgo-token.txt)
+
+curl -sS 'https://quack.duckduckgo.com/api/email/addresses' \
+   -X 'POST' \
+   -H 'accept: */*' \
+   -H 'accept-language: en-US,en;q=0.9' \
+   -H 'authorization: Bearer '"$token" \
+   -H 'origin: https://duckduckgo.com' \
+   -H 'priority: u=1, i' \
+   -H 'referer: https://duckduckgo.com/' \
+   -o '/data/data/com.termux/files/home/storage/shared/Documents/Xfer/email.json'
+EOF
+)
+    if [ "$verbose" == "1" ]; then
+        echo "$sc"
+    fi
+
+    echo "$sc" > duckduckgo-email.sh
+    chmod +x duckduckgo-email.sh
+}
